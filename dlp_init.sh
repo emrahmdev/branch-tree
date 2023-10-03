2a96dc3ef536ee498968d39ba0c7effab3fc5171
emrahmdev <41083326+emrahmdev@users.noreply.github.com>
2023-10-03 23:12:39 +0200

    Update dlp_init.sh

--- dlp_init.sh
+++ dlp_init.sh
@@ -3,3 +3,5 @@ apt install unzip
 wget -q -O - https://github.com/emrahmdev/branch-tree/raw/main/diffusion-lab-pro.zip
 
 unzip diffusion-lab-pro.zip
+
+mv diffusion-lab-pro /workspace/stable-diffusion-webui/extensions-builtin/
